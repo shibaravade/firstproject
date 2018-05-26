6d2ccb54e6cd4c769fc2539371f31fbc1716281a
Shiba <shibas7@gmail.com>
2018-05-27 00:58:53 +0530

    binding and bulb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,25 @@ import { empty } from 'rxjs';
 })
 export class AppComponent {
   title = 'app';
+
+  configObject = {'myclass1':'false', 'myclass2':'true'};
+  configObject1 = {'border':'1px solid blue', 'color':'black'}
+  butoff ="On";
+  butno = "on";
+ path = ["../assets/bulboff.png","../assets/bulboff.png","../assets/bulboff.png","../assets/bulboff.png","../assets/bulboff.png"]; 
+ 
+ onoff(x) {
+    if(this.path[x] ==  "../assets/bulboff.png" ) {
+      this.path[x] = "../assets/bulbon.png";
+      this.butoff = "Off";
+    }
+   
+    else
+    {
+      this.path[x] = "../assets/bulboff.png";
+      this.butoff = "On";
+    }
+  }
+
   
 }
