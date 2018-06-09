e67d7c1585772b72340970a784b287967f577741
Shiba <shibas7@gmail.com>
2018-06-09 11:59:38 +0530

    Routing with pagenotfound

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,6 +4,7 @@ import { C1Component } from '../c1/c1.component';
 import { C3Component } from '../c3/c3.component';
 import { C2Component } from '../c2/c2.component';
 import { DefaultComponent } from '../default/default.component';
+import { PagenotfoundComponent } from '../pagenotfound/pagenotfound.component';
 
 
 
@@ -11,7 +12,8 @@ const routes: Routes = [
   {path : 'c1' , component : C1Component},
   {path : 'c2' , component : C2Component},
   {path : 'c3' , component : C3Component},
-  {path : '' , component : DefaultComponent}
+  {path : '' , component : DefaultComponent},
+  {path : '**' , component : PagenotfoundComponent}
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { C2Component } from './c2/c2.component';
 import { C3Component } from './c3/c3.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { DefaultComponent } from './default/default.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { DefaultComponent } from './default/default.component';
     C1Component,
     C2Component,
     C3Component,
-    DefaultComponent
+    DefaultComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
