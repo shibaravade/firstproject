bc8999aae9c96e3159a5d03dd86573f46d165a81
Shiba <shibas7@gmail.com>
2018-06-09 11:52:05 +0530

    Routing Class

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { C1Component } from '../c1/c1.component';
+import { C3Component } from '../c3/c3.component';
+import { C2Component } from '../c2/c2.component';
+import { DefaultComponent } from '../default/default.component';
+
+
+
+const routes: Routes = [
+  {path : 'c1' , component : C1Component},
+  {path : 'c2' , component : C2Component},
+  {path : 'c3' , component : C3Component},
+  {path : '' , component : DefaultComponent}
+  
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { FormsModule } from '@angular/forms' ;
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { C1Component } from './c1/c1.component';
+import { C2Component } from './c2/c2.component';
+import { C3Component } from './c3/c3.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { DefaultComponent } from './default/default.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    C1Component,
+    C2Component,
+    C3Component,
+    DefaultComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
