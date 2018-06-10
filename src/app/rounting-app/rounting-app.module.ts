70abc3ae17f29d96370531c1e16aa0c59594acc5
Shiba <shibas7@gmail.com>
2018-06-10 10:35:09 +0530

    assng1

---
+++ src/app/rounting-app/rounting-app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AppRouting, Routes } from '@angular/routes';
+
+@NgModule({
+  imports: [
+    AppRouting
+  ],
+  declarations: []
+})
+export class RountingAppModule { }
