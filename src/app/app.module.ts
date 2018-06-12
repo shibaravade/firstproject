293fd1078d0088ac96f7f3516514dcd3c3b0ea85
Shiba <shibas7@gmail.com>
2018-06-13 01:06:05 +0530

    first submission

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { FormsModule } from '@angular/forms' ;
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { DefaultComponent } from './default/default.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    DefaultComponent
    
   ],
   imports: [
---
+++ src/app/approuting/approuting.module.spec.ts
@@ -0,0 +1,13 @@
+import { AppRoutingModule } from './approuting.module';
+
+describe('AppRoutingModule', () => {
+  let appRoutingModule: AppRoutingModule;
+
+  beforeEach(() => {
+    appRoutingModule = new AppRoutingModule();
+  });
+
+  it('should create an instance', () => {
+    expect(appRoutingModule).toBeTruthy();
+  });
+});
